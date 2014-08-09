fe3a999f0cd16ed9308b6871dfea3ac9eafb325c
slmnn <arto.salminen@gmail.com>
2014-08-09 17:58:18 +0300

    heroku deploy

--- config/models.js
+++ config/models.js
@@ -6,7 +6,7 @@
  * in each of your models.
  */
 
-module.exports.model = {
+module.exports.models = {
 
   // Your app's default connection.
   // i.e. the name of one of your app's connections (see `config/connections.js`)
